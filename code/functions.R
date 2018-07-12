697437b3c43ccb8c5cfda2e2446210b3449b168c
Steven Davenport <stevenseandavenport@gmail.com>
2018-07-12 10:07:30 +0100

    added document to topcode/explore conversions

--- code/functions.R
+++ code/functions.R
@@ -1,5 +1,6 @@
 #functions
 invtypekey <- read_csv('../data/invtype_legend.csv')
+
 # function: get invtype from inventorytype
 getInvtype <- function(inventorytype) {
   plyr:: mapvalues(
@@ -9,3 +10,13 @@ getInvtype <- function(inventorytype) {
     warn_missing = FALSE)
 }
 
+# Make date column from a set of year-month columns
+make_date <- function(yyyy, m, quarter=F) {
+  # we need this for negative substring indexing
+  require(stringr)
+  # year is 4 digits; month is either 1 or 2 digits.
+  # Correct month by prefixing 0, getting all but last digit
+  month <- str_sub(paste0(0, m), -2) 
+  
+  as.Date(paste0(yyyy, "-", month, "-01"))
+}
